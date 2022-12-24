080dc4e6ce396438180d469317bf2e5606f70370
rizky MSI <rizfirsy@yahoo.com>
2022-12-24 12:24:44 +0700

    add biodata

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-$("#cancel").on("click", function (event) {
-  $("#input").text(" ");
-});
-
-$(".daftar").on("click", (e) => {
+$("button.daftar").click((e) => {
   e.preventDefault();
-  window.alert("Form akan terkirim! Terimakasih telah mendaftar!");
+
+  if ($("input").val() === "") {
+    return alert("Input tidak boleh kosong!");
+  }
+  $("form").empty();
+  $("form").html(
+    `<h1>Terimakasih!</h1>
+    <br>
+    <p>Data telah terkirim dan akan tervalidasi kurang dari 5 menit!</p>
+    <br>
+    <button>Isi form lagi</button>
+    `
+  );
 });
